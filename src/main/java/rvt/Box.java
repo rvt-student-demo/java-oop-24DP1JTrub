a076b22e354c97612406de2440b2f6c13259ffad
24DP1JTrub <a240530jt@rvt.lv>
2026-03-10 21:21:43 +0200

    pagaidam tukšs box konstruktors

---
+++ src/main/java/rvt/Box.java
@@ -0,0 +1,19 @@
+package rvt;
+
+import java.util.ArrayList;
+
+public class Box {
+
+    private double capacity;
+    private ArrayList<Packable> items;
+
+    public Box(double capacity) {
+        this.capacity = capacity;
+        this.items = new ArrayList<>();
+    }
+
+    public void add(Packable item) {
+
+    }
+
+}
