f42eac64b9f58c199312be541a30a5270df66c2a
24DP1JTrub <a240530jt@rvt.lv>
2026-03-10 21:25:31 +0200

    izveidoju box funkcionalitāti

--- src/main/java/rvt/Box.java
+++ src/main/java/rvt/Box.java
@@ -12,8 +12,25 @@ public class Box {
         this.items = new ArrayList<>();
     }
 
+    public double weight() {
+    double total = 0;
+
+    for (Packable item : items) {
+        total += item.weight();
+    }
+
+    return total;
+}
     public void add(Packable item) {
 
+    if (weight() + item.weight() <= capacity) {
+        items.add(item);
+    }
+}
+
+    @Override
+    public String toString() {
+        return "Box: " + items.size() + " items, total weight " + weight() + " kg";
     }
 
 }
