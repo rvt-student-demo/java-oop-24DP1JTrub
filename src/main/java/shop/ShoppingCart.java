51f8ddfdeaf272d3954997c8c161589379dd0ed4
24DP1JTrub <a240530jt@rvt.lv>
2026-03-16 17:56:00 +0200

    part 5

---
+++ src/main/java/shop/ShoppingCart.java
@@ -0,0 +1,34 @@
+package shop;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingCart {
+    private Map<String, Item> items;
+
+    public ShoppingCart() {
+        this.items = new HashMap<>();
+    }
+
+    public void add(String product, int price) {
+        if (this.items.containsKey(product)) {
+            this.items.get(product).increaseQuantity();
+            return;
+        }
+        this.items.put(product, new Item(product, 1, price));
+    }
+
+    public int price() {
+        int total = 0;
+        for (Item item : this.items.values()) {
+            total += item.price();
+        }
+        return total;
+    }
+
+    public void print() {
+        for (Item item : this.items.values()) {
+            System.out.println(item);
+        }
+    }
+}
